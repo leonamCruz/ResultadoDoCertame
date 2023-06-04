29dea6ab56716184554fc73c3a69649c9386a8e1
leonam <leonam447@gmail.com>
2023-06-04 17:16:41 -0300

    View Leitor

---
+++ app/src/main/java/tech/leonam/resultadodocertame/view/CriarProva.java
@@ -0,0 +1,16 @@
+package tech.leonam.resultadodocertame.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tech.leonam.resultadodocertame.R;
+
+public class CriarProva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_prova);
+    }
+}
