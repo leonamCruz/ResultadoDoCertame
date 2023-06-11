844ffa5427f0d1579ee89666b57d7c8d9d0d4a98
leonam <leonam447@gmail.com>
2023-06-11 19:07:00 -0300

    Add View Reciclavel

--- app/src/main/java/tech/leonam/resultadodocertame/model/repository/CadastraTurmaDao.java
+++ app/src/main/java/tech/leonam/resultadodocertame/model/repository/CadastraTurmaDao.java
@@ -6,21 +6,17 @@ import android.content.Context;
 import tech.leonam.resultadodocertame.model.entidade.TurmaEntidade;
 
 public class CadastraTurmaDao {
-    private TurmaEntidade turmaEntidade;
-    private Context context;
-
     public static boolean cadastrar(TurmaEntidade turmaEntidade, Context context) {
         try {
             var nomeTb = turmaEntidade.getNomeDaTurma().replace(" ","_");
             var bd = new CreateDataBase(context).getWritableDatabase();
-            var query = "CREATE TABLE IF NOT EXISTS " + nomeTb + " (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, nota REAL)";
+            var query = "CREATE TABLE IF NOT EXISTS " + nomeTb + " (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)";
             bd.execSQL(query);
 
             var valores = new ContentValues();
             for(var i = 0 ; i < turmaEntidade.getTurma().size(); i++){
                 valores.clear();
                 valores.put("nome", turmaEntidade.getTurma().get(i).getNome());
-                System.out.println(turmaEntidade.getTurma().get(i).getNome());
                 bd.insert(nomeTb,null,valores);
             }
             bd.close();
---
+++ app/src/main/java/tech/leonam/resultadodocertame/view/InfoTurmaView.java
@@ -0,0 +1,35 @@
+package tech.leonam.resultadodocertame.view;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import tech.leonam.resultadodocertame.R;
+import tech.leonam.resultadodocertame.service.PegaTurmas;
+import tech.leonam.resultadodocertame.view.adpters.ReciclerViewAdapter;
+
+public class InfoTurmaView extends AppCompatActivity {
+    private ReciclerViewAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_turma_view);
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(Color.BLACK);
+        iniciarReciclagem();
+    }
+
+    public void iniciarReciclagem() {
+        RecyclerView recyclerView = findViewById(R.id.reciclavel);
+        adapter = new ReciclerViewAdapter(this, PegaTurmas.turmas(this));
+        var layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
