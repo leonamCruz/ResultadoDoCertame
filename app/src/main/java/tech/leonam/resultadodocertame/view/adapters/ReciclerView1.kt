710cfdc0f9fb4b1b5ec235e0d40b9cca030bc63a
leonam <leonam447@gmail.com>
2023-06-24 13:49:11 -0300

    Recicler View em Kotlin

--- app/src/main/java/tech/leonam/resultadodocertame/view/adapters/ReciclerView1.kt
+++ app/src/main/java/tech/leonam/resultadodocertame/view/adapters/ReciclerView1.kt
@@ -1,16 +1,9 @@
 package tech.leonam.resultadodocertame.view.adapters
 
 import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import tech.leonam.resultadodocertame.R
+import tech.leonam.resultadodocertame.databinding.TurmasBinding
 
 class ReciclerView(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val nomeDaTurma: TextView
-    val qntdDeAlunos: TextView
-
-    init {
-        nomeDaTurma = itemView.findViewById(R.id.nome_turma_reciclavel)
-        qntdDeAlunos = itemView.findViewById(R.id.qntd_de_alunos_reciclavel)
-    }
+    var binding: TurmasBinding = TurmasBinding.bind(itemView)
 }
