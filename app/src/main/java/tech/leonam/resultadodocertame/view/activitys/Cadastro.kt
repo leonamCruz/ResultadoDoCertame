d0555be72f805ed67be1820101a839d5b18e6c63
leonam <leonam447@gmail.com>
2023-06-24 13:47:13 -0300

    Cadastro em Kotlin

--- app/src/main/java/tech/leonam/resultadodocertame/view/activitys/Cadastro.kt
+++ app/src/main/java/tech/leonam/resultadodocertame/view/activitys/Cadastro.kt
@@ -3,49 +3,34 @@ package tech.leonam.resultadodocertame.view.activitys
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import tech.leonam.resultadodocertame.R
+import tech.leonam.resultadodocertame.databinding.ActivityCadastroBinding
 
 class Cadastro : AppCompatActivity() {
-    private var cadastro: Button? = null
-    private var email: EditText? = null
-    private var senha: EditText? = null
-    private var mAuth: FirebaseAuth? = null
-    private val user: FirebaseUser? = null
+    private lateinit var binding: ActivityCadastroBinding
+    private var mAuth: FirebaseAuth? = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
         supportActionBar!!.hide()
         window.statusBarColor = Color.BLACK
         setContentView(R.layout.activity_cadastro)
-        iniciarComponentes()
         cadastrar()
     }
 
-    fun iniciarComponentes() {
-        cadastro = findViewById(R.id.botaoCadastro)
-        email = findViewById(R.id.emailCadastro)
-        senha = findViewById(R.id.senhaCadastro)
-        mAuth = FirebaseAuth.getInstance()
-    }
-
-    fun cadastrar() {
-        cadastro!!.setOnClickListener { e: View? ->
+    private fun cadastrar() {
+        binding.botaoCadastro.setOnClickListener {
             mAuth!!.createUserWithEmailAndPassword(
-                email!!.text.toString(), senha!!.text.toString()
+                binding.emailCadastro.text.toString(), binding.senhaCadastro.text.toString()
             ).addOnCompleteListener(this) { task: Task<AuthResult?> ->
-                if (task.isSuccessful) {
+                if (task.isSuccessful)
                     startActivity(Intent(this, Logar::class.java))
-                } else {
-                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
-                }
+                else Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
