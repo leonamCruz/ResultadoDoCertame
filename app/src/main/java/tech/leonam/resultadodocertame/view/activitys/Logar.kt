62788b631ecddaec55b83bb152db31eac8d35b49
leonam <leonam447@gmail.com>
2023-06-24 13:17:20 -0300

    Logar Em Kotlin

--- app/src/main/java/tech/leonam/resultadodocertame/view/activitys/Logar.kt
+++ app/src/main/java/tech/leonam/resultadodocertame/view/activitys/Logar.kt
@@ -3,70 +3,48 @@ package tech.leonam.resultadodocertame.view.activitys
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import tech.leonam.resultadodocertame.R
+import tech.leonam.resultadodocertame.databinding.ActivityLogarBinding
 
 class Logar : AppCompatActivity() {
-    private var logar: Button? = null
-    private var recuperarSenha: Button? = null
-    private var email: EditText? = null
-    private var senha: EditText? = null
-    private var cadastro: TextView? = null
-    private var mAuth: FirebaseAuth? = null
-    private val user: FirebaseUser? = null
+    private var mAuth: FirebaseAuth? = FirebaseAuth.getInstance()
+    private lateinit var binding: ActivityLogarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logar)
+        binding = ActivityLogarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar!!.hide()
         window.statusBarColor = Color.BLACK
-        iniciarComponentes()
         logar()
         cadastrar()
         recuperarSenha()
     }
 
-    fun iniciarComponentes() {
-        logar = findViewById(R.id.botaoLogar)
-        recuperarSenha = findViewById(R.id.botaoRecuperarSenha)
-        email = findViewById(R.id.emailLogar)
-        senha = findViewById(R.id.senhaLogar)
-        cadastro = findViewById(R.id.cadastrar)
-        mAuth = FirebaseAuth.getInstance()
-    }
-
     override fun onStart() {
         super.onStart()
-        val currentUser = mAuth!!.currentUser
-        if (currentUser != null) {
-            abrirMenuPrincipal()
-        }
+        if (mAuth?.currentUser != null) abrirMenuPrincipal()
     }
 
-    fun logar() {
-        logar!!.setOnClickListener { e: View? ->
+    private fun logar() {
+        binding.botaoLogar.setOnClickListener {
             mAuth!!.signInWithEmailAndPassword(
-                email!!.text.toString(), senha!!.text.toString()
+                binding.emailLogar.text.toString(), binding.senhaLogar.text.toString()
             ).addOnCompleteListener(this) { task: Task<AuthResult?> ->
-                val tarefa = if (task.isSuccessful) abrirMenuPrincipal() else abrirErroDeLogin()
+                if (task.isSuccessful) abrirMenuPrincipal() else abrirErroDeLogin()
             }
         }
     }
 
-    fun abrirMenuPrincipal(): Boolean {
+    private fun abrirMenuPrincipal(): Boolean {
         startActivity(Intent(this, MainActivity::class.java))
         return true
     }
 
-    fun abrirErroDeLogin(): Boolean {
+    private fun abrirErroDeLogin(): Boolean {
         val dialog = AlertDialog.Builder(this@Logar)
         dialog.setTitle("Infelizmente não foi possível efetuar login")
         dialog.setMessage("Verifique seu Email ou Senha.")
@@ -74,8 +52,8 @@ class Logar : AppCompatActivity() {
         return true
     }
 
-    fun cadastrar() {
-        cadastro!!.setOnClickListener { e: View? ->
+    private fun cadastrar() {
+        binding.cadastrar.setOnClickListener {
             startActivity(
                 Intent(
                     this,
@@ -85,7 +63,7 @@ class Logar : AppCompatActivity() {
         }
     }
 
-    fun recuperarSenha() {
-        recuperarSenha!!.setOnClickListener { e: View? -> }
+    private fun recuperarSenha() {
+        binding.botaoRecuperarSenha.setOnClickListener{}
     }
 }
