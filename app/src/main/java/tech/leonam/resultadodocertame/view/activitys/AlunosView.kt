8e47a3cffecc0fd1d369caee79d9f9e2e3f8aa6c
leonam <leonam447@gmail.com>
2023-06-24 14:27:05 -0300

    Toda View em Kotlin

--- app/src/main/java/tech/leonam/resultadodocertame/view/activitys/AlunosView.kt
+++ app/src/main/java/tech/leonam/resultadodocertame/view/activitys/AlunosView.kt
@@ -3,58 +3,39 @@ package tech.leonam.resultadodocertame.view.activitys
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.initialization.InitializationStatus
 import tech.leonam.resultadodocertame.R
+import tech.leonam.resultadodocertame.databinding.ActivityAlunosViewBinding
 import tech.leonam.resultadodocertame.model.entidade.AlunoEntidade
 import tech.leonam.resultadodocertame.modelView.service.AlunoService
 import tech.leonam.resultadodocertame.modelView.service.ControleDeTurmaService
 import tech.leonam.resultadodocertame.modelView.service.TurmaService
 
 class AlunosView : AppCompatActivity() {
-    private var nomeDaClasse: String? = null
-    private var nomesAlunos: EditText? = null
-    private var criarAlunos: Button? = null
-    private var ads: AdView? = null
+    private lateinit var binding: ActivityAlunosViewBinding
+    private lateinit var nomeDaClasse: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alunos_view)
         supportActionBar!!.hide()
         window.navigationBarColor = Color.BLACK
-        nomeDaClasse = intent.getStringExtra("nomeDaClasse")
-        iniciarComponentes()
-        //TODO iniciar somente quando for para produção
-        //iniciarAds();
-        setarHint()
+        nomeDaClasse = intent.getStringExtra("nomeDaClasse").toString()
+        binding.alunosEditText.hint = getString(R.string.a_turma) + nomeDaClasse + getString(R.string.est_sendo_criada_por_favor_insira_cada_aluno_em_uma_linha_diferente)
         clickBotao()
     }
-
-    fun iniciarComponentes() {
-        nomesAlunos = findViewById(R.id.alunosEditText)
-        criarAlunos = findViewById(R.id.adicionarAlunos)
-        ads = findViewById(R.id.adView)
+    private fun iniciarAds() {
+        MobileAds.initialize(this)
+        binding.adView.loadAd( AdRequest.Builder().build())
     }
 
-    fun iniciarAds() {
-        MobileAds.initialize(this) { initializationStatus: InitializationStatus? -> }
-        val request = AdRequest.Builder().build()
-        if (request != null) {
-            ads!!.loadAd(request)
-        }
-    }
-
-    fun clickBotao() {
-        criarAlunos!!.setOnClickListener { e: View? ->
-            val nomeAlunos = nomesAlunos!!.text.toString()
-            val alunos =
-                nomeAlunos.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    private fun clickBotao() {
+        binding.adicionarAlunos.setOnClickListener {
+            val nomeAlunos = binding.alunosEditText.text.toString()
+            val alunos = nomeAlunos.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
             val lista = ArrayList<AlunoEntidade>()
             for (aluno in alunos) {
                 val entidade = AlunoService()
@@ -77,10 +58,5 @@ class AlunosView : AppCompatActivity() {
                 ).show()
             }
         }
-    }
-
-    fun setarHint() {
-        nomesAlunos!!.hint =
-            getString(R.string.a_turma) + nomeDaClasse + getString(R.string.est_sendo_criada_por_favor_insira_cada_aluno_em_uma_linha_diferente)
     }
 }
--- app/src/main/java/tech/leonam/resultadodocertame/view/activitys/CriarProva.kt
+++ app/src/main/java/tech/leonam/resultadodocertame/view/activitys/CriarProva.kt
@@ -3,71 +3,31 @@ package tech.leonam.resultadodocertame.view.activitys
 import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.initialization.InitializationStatus
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import tech.leonam.resultadodocertame.R
+import tech.leonam.resultadodocertame.databinding.ActivityCriarProvaBinding
 import tech.leonam.resultadodocertame.modelView.service.ConfigProvaService
 import tech.leonam.resultadodocertame.modelView.service.SalvaProvasService
 import tech.leonam.resultadodocertame.modelView.util.CriacaoDePdf
 
 class CriarProva : AppCompatActivity() {
-    private var qntAlternativas: EditText? = null
-    private var qntDeQuestoes: EditText? = null
-    private var nomeDaTurma: EditText? = null
-    private var alternativasCorretas: EditText? = null
-    private var identificacaoProva: EditText? = null
-    private var isIndividual: CheckBox? = null
-    private var criarProva: Button? = null
+    private lateinit var binding: ActivityCriarProvaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MobileAds.initialize(this) { initializationStatus: InitializationStatus? -> }
-        setContentView(R.layout.activity_criar_prova)
+        binding = ActivityCriarProvaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar!!.hide()
         window.navigationBarColor = Color.BLACK
-        iniciarComponentes()
-        //HABILITAR SOMENTE QUANDO FOR PARA PRODUCAO
-        // TODO iniciarAnuncio(new AdRequest.Builder().build());
         criarProva()
     }
 
-    fun iniciarComponentes() {
-        qntAlternativas = findViewById(R.id.txtQntDeAlternativas)
-        qntDeQuestoes = findViewById(R.id.txtQntDeQuestao)
-        nomeDaTurma = findViewById(R.id.txtTurma)
-        alternativasCorretas = findViewById(R.id.txtQuestoesCorretas)
-        isIndividual = findViewById(R.id.checkIndividual)
-        criarProva = findViewById(R.id.botaoCriarProva)
-        identificacaoProva = findViewById(R.id.identificacaoCriarProva)
-    }
-
-    fun iniciarAnuncio(request: AdRequest?) {
-        try {
-            InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", request!!,
-                object : InterstitialAdLoadCallback() {
-                    override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                        interstitialAd.show(this@CriarProva)
-                    }
-                })
-        } catch (ignored: Exception) {
-        }
-    }
-
-    fun criarProva() {
-        criarProva!!.setOnClickListener { e: View? ->
+    private fun criarProva() {
+        binding.botaoCriarProva.setOnClickListener {
             val qntdDeCorretasDigitadas =
-                alternativasCorretas!!.text.toString().split("\n".toRegex())
+                binding.txtQntDeAlternativas.text.toString().split("\n".toRegex())
                     .dropLastWhile { it.isEmpty() }
                     .toTypedArray().size
-            if (qntAlternativas!!.text.toString().toInt() != qntdDeCorretasDigitadas) {
+            if (binding.txtQntDeAlternativas.text.toString().toInt() != qntdDeCorretasDigitadas) {
                 val dialog = AlertDialog.Builder(this)
                 dialog.setTitle("Erro")
                 dialog.setMessage("A quantidade de alternativas está diferente da quantidade de alternativas corretas que você forneceu")
@@ -75,13 +35,12 @@ class CriarProva : AppCompatActivity() {
                 dialog.create().show()
             } else {
                 val configs = ConfigProvaService()
-                configs.isIndividual = isIndividual!!.isChecked
-                configs.alternativasCorretas = alternativasCorretas!!.text.toString()
-                configs.qntDeQuestoes = qntDeQuestoes!!.text.toString()
-                configs.qntAlternativas = qntAlternativas!!.text.toString()
-                configs.nomeDaTurma = nomeDaTurma!!.text.toString()
-                configs.identificacaoProva = identificacaoProva!!.text.toString()
-                println(configs.identificacaoProva)
+                configs.isIndividual = binding.checkIndividual.isChecked
+                configs.alternativasCorretas = binding.txtQuestoesCorretas.text.toString()
+                configs.qntDeQuestoes =binding.txtQntDeQuestao.text.toString()
+                configs.qntAlternativas = binding.txtQntDeAlternativas.text.toString()
+                configs.nomeDaTurma = binding.txtTurma.text.toString()
+                configs.identificacaoProva = binding.identificacaoCriarProva.text.toString()
                 try {
                     CriacaoDePdf(configs).criaPdf(this)
                     SalvaProvasService().salvaProvas(this, configs)
